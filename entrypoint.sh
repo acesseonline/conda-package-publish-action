b696fa6d34cff5106feea14145b19bd2e3b14173
Jadson Ribeiro <rjadson@outlook.com>
2020-12-09 00:09:55 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,7 @@ set -o pipefail
 
 go_to_build_dir() {
     if [ ! -z $INPUT_SUBDIR ]; then
-        cd $INPUT_SUBDIR
-    fi
-}
-
-check_if_meta_yaml_file_exists() {
-    if [ ! -f meta.yaml ]; then
-        echo "meta.yaml must exist in the directory that is being packaged and published."
-        exit 1
+        ls
     fi
 }
 
@@ -28,6 +21,5 @@ upload_package(){
 }
 
 go_to_build_dir
-check_if_meta_yaml_file_exists
 build_package
 upload_package
